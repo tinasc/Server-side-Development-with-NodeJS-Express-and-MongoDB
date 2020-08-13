cd0273b7941537bc57903a7a5c2062ab851ff597
Tina <tinasylviac@gmail.com>
2020-08-13 13:52:23 +0530

    Passport

--- models/users.js
+++ models/users.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+/*var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+}); */
+
+User.plugin(passportLocalMongoose);
 
 var User = new Schema({
     username: {
